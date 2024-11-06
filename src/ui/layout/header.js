c22914f000404de5f82008fd57cc232e7e275dfa
Joao1V <joaovm.dev@gmail.com>
2024-11-06 16:41:29 -0400

    comentado opções no header

--- src/ui/layout/header.js
+++ src/ui/layout/header.js
@@ -55,7 +55,7 @@ const Header = () => {
         <header className={`header-transparent  header-sticky ${pathname !== "/" ? "theme_bg" : "sasup-header-style-5"}`}>
             <div className="header-main">
                 <div className="container">
-                    <div className="row align-items-center justify-content-between">
+                    <div className="row align-items-center justify-content-between py-4">
                         <div className="col-xxl-3 col-xl-3 col-lg-3 col-md-4 col-6">
                             <div className="sasup-logo mr-35 d-inline-block">
                                 <Link href="/" className="logo-1">
@@ -68,42 +68,42 @@ const Header = () => {
                                 </Link>
                             </div>
                         </div>
-                        <div className="col-xxl-6 col-xl-6 col-lg-6 d-none d-lg-block text-center">
-                            <div className="sasup-header d-none d-lg-inline-block">
-                                <nav id="mobile-menu">
-                                    <ul>
-                                        <li>
-                                            <a href="index.html">Home</a>
-                                            <ul className="sub-menu">
-                                                <li>
-                                                    <Link href={"#"}>
-                                                        Teste
-                                                    </Link>
-                                                </li>
-                                                <li>
-                                                    <Link href={"#"}>
-                                                        Teste 2
-                                                    </Link>
-                                                </li>
-                                            </ul>
-                                        </li>
-                                        <li>
-                                            <Link href="#">Page</Link>
-                                        </li>
-                                        <li>
-                                            <Link href="#">Pricing</Link>
-                                        </li>
-                                        <li>
-                                            <Link href="#">Blog</Link>
+                        {/*<div className="col-xxl-6 col-xl-6 col-lg-6 d-none d-lg-block text-center">*/}
+                        {/*    <div className="sasup-header d-none d-lg-inline-block">*/}
+                        {/*        <nav id="mobile-menu">*/}
+                        {/*            <ul>*/}
+                        {/*                <li>*/}
+                        {/*                    <a href="index.html">Home</a>*/}
+                        {/*                    <ul className="sub-menu">*/}
+                        {/*                        <li>*/}
+                        {/*                            <Link href={"#"}>*/}
+                        {/*                                Teste*/}
+                        {/*                            </Link>*/}
+                        {/*                        </li>*/}
+                        {/*                        <li>*/}
+                        {/*                            <Link href={"#"}>*/}
+                        {/*                                Teste 2*/}
+                        {/*                            </Link>*/}
+                        {/*                        </li>*/}
+                        {/*                    </ul>*/}
+                        {/*                </li>*/}
+                        {/*                <li>*/}
+                        {/*                    <Link href="#">Page</Link>*/}
+                        {/*                </li>*/}
+                        {/*                <li>*/}
+                        {/*                    <Link href="#">Pricing</Link>*/}
+                        {/*                </li>*/}
+                        {/*                <li>*/}
+                        {/*                    <Link href="#">Blog</Link>*/}
 
-                                        </li>
-                                        <li>
-                                            <Link href="#">Contact</Link>
-                                        </li>
-                                    </ul>
-                                </nav>
-                            </div>
-                        </div>
+                        {/*                </li>*/}
+                        {/*                <li>*/}
+                        {/*                    <Link href="#">Contact</Link>*/}
+                        {/*                </li>*/}
+                        {/*            </ul>*/}
+                        {/*        </nav>*/}
+                        {/*    </div>*/}
+                        {/*</div>*/}
                         <div className="col-xxl-3 col-xl-3 col-lg-3 col-md-8 col-6">
                             <div className="sasup-header-action-5 text-end">
                                 <Link href="#" className={pathname === "/" ? "sasup-login-btn-5" : "sasup-transparent-btn"}>
