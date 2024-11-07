e0cb77c0b348372b80703ec6e89839acd4705494
Joao1V <joaovm.dev@gmail.com>
2024-11-07 11:05:48 -0400

    removido wizard da pasta components

--- src/ui/components/builder/wizard-builder/components/button-next.js
+++
@@ -1,9 +0,0 @@
-"use client"
-
-export function ButtonNext(props) {
-    return (
-        <button className={`btn btn-${props.type || "secondary"} col-12 col-md-4 col-lg-3`} {...props}>
-            {props.title || "Próximo"}
-        </button>
-    )
-}
