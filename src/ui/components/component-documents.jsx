d8400018511fcb13efb22db1594e7d7ca8f1e4d5
Joao1V <joaovm.dev@gmail.com>
2024-11-12 11:51:01 -0400

    fluxo completo de documentos

--- src/ui/components/component-documents.jsx
+++ src/ui/components/component-documents.jsx
@@ -1,101 +1,270 @@
 "use client";
 
-import { useGetData } from "@/hooks/useGetData";
+import {useGetData} from "@/hooks/useGetData";
 import React, { useState } from "react";
-import ComponentSelectFile from "@/ui/components/component-select-file";
-import { KTIcon } from "@/libs/KTIcon/KTIcon";
+import {KTIcon} from "@/libs/KTIcon/KTIcon";
+import api from "@/services/api";
+import {useMutation} from "@tanstack/react-query";
+import ComponentDocumentDetails from "@/ui/components/component-document-details";
+import _ from "lodash";
+import {ComponentDocumentFilter} from "@/ui/components/component-document-filter";
+import ComponentDocumentApproved from "@/ui/components/component-document-approved";
 
-const status = {
-    "PENDING-UPLOAD": "warning",
-}
+const PENDING_UPLOAD = "PENDING-UPLOAD";
+const WAITING_VALIDATION = "WAITING-VALIDATION";
+const APPROVED = "APPROVED";
+const RETURNED = "RETURNED";
+
+const statusDocument = {
+    [PENDING_UPLOAD]: {
+        key: "PENDING-UPLOAD",
+        color: "warning",
+        label: "Pendente"
+    },
+    [WAITING_VALIDATION]: {
+        key: "WAITING-VALIDATION",
+        color: "primary",
+        label: "Anexados"
+    },
+    [APPROVED]: {
+        key: "APPROVED",
+        color: "success",
+        label: "Aprovado(s)"
+    },
+    [RETURNED]: {
+        key: "RETURNED",
+        color: "danger",
+        label: "Recusado(s)"
+    }
+};
+
+const allStatus = Object.keys(statusDocument).map(key => statusDocument[key].key);
+
+const statusToFilter = Object.keys(statusDocument).map(key => ({
+    label: statusDocument[key].label,
+    value: statusDocument[key].key
+}));
+
 export default function ComponentDocuments(props) {
+    const [status, setStatus] = useState(props.serverData.initialStatus || PENDING_UPLOAD);
     const [activeDocument, setActiveDocument] = useState({});
-    const [colorBadge, setColorBadge] = useState("")
 
-    const onChangeActiveDocument = (data, index) => {
-        const aux = data.object.entity_documents[index];
-        onChangeBadgeColor(aux.status);
-        setActiveDocument(aux)
+    const onChangeActiveDocument = (item) => {
+        setActiveDocument({...item})
+    }
+
+    const filterResponseFn = (data) => {
+
+        const sortedByStatus = {};
+        const countStatus = {};
+        const statusProcess =  {
+            isDraft: data?.status === "DRAFT",
+            isWaitingValidation: data?.status === "WAITING-VALIDATION",
+            isValidating: data?.status === "VALIDATING",
+            isReturned: data?.status === "RETURNED",
+            isApproved: data?.status === "APPROVED",
+        };
+
+        allStatus.forEach((value) => {
+            sortedByStatus[value] = data.entity_documents.filter((item) => item.status === value)
+            sortedByStatus[value] = _.orderBy(sortedByStatus[value], ['document_type_id'], ['asc']);
+            countStatus[value] = sortedByStatus[value].length
+        });
+
+        return {
+            sortedByStatus,
+            countStatus,
+            statusProcess
+        }
+    }
+
+    const handleSubmitValidation = async () => {
+        try {
+
+            if (countStatus[PENDING_UPLOAD] === 0) {
+                await api.put(`/restrict/entity-flow/${identifier}`, {
+                    status: "WAITING-VALIDATION",
+                })
+                await refetch()
+            }
+
+        } catch (e) {
+
+        }
     }
 
     const isActiveMenu = (id) => {
         if (activeDocument && (id === activeDocument?.document_type_id)) {
-            return "text-secondary bg-light p-3 rounded-2 fw-bold"
+            return "fw-bold "
         }
-        return "text-gray-600 px-3"
+        return "text-gray-600 "
     }
 
-    const onChangeBadgeColor = (key) => {
-        let color = status[key];
-        setColorBadge(color);
-    }
+    const statusDocumentToDisplay = () => {
+        if (statusProcess?.isDraft) {
+            return [PENDING_UPLOAD, WAITING_VALIDATION];
+        } else if (statusProcess?.isReturned || statusProcess?.isWaitingValidation || statusProcess.isValidating) {
+            return [WAITING_VALIDATION, APPROVED, RETURNED];
+        }
+        return [];
+    };
 
-    const BadgeStatus = (props) => {
-        const { status } = props
-
+    function getStatusProcessText(status) {
         switch (status) {
-            case "PENDING-UPLOAD":
-                 break;
+            case "DRAFT":
+                return "Aguardando anexo de documentos";
+            case "WAITING-VALIDATION":
+                return "O processo está aguardando validação";
+            case "VALIDATING":
+                return "O processo está em validação.";
+            case "RETURNED":
+                return "O processo foi restituído.";
             default:
-                return null;
+                return "";
         }
-
-        return (
-            <div className={`btn btn-light-${colorBadge} d-flex flex-center`}>
-                <KTIcon name={"time"} type={"outline"} className={"fs-5"}/>
-                <span>Pendente</span>
-            </div>
-        )
-
     }
 
     const { data: identifier } = useGetData({
         queryKey: ["/restrict/entity-flow/paginate"],
         url: "/restrict/entity-flow/paginate",
         onSuccess: (response) => {
-            if (response.object) {
-                return response.object?.data[0].identifier;
+            if (response) {
+                return response?.data[0].identifier;
             }
         },
         serverData: props.serverData.entityFlowProcessIdentifier,
     });
 
-    const { data } = useGetData({
+    const { data, refetch } = useGetData({
         queryKey: ["/restrict/entity-flow", identifier],
         url: `/restrict/entity-flow/${identifier}`,
         onSuccess: (response) => {
-            onChangeActiveDocument(response, 0)
+            const filtered = filterResponseFn(response.object);
+            const { statusProcess, countStatus, sortedByStatus } = filtered;
+
+            const isReturned = statusProcess.isReturned;
+            const returnedCount = countStatus[RETURNED];
+            const waitingValidation = sortedByStatus[WAITING_VALIDATION];
+            const returnedStatus = sortedByStatus[RETURNED];
+
+            if (isReturned) {
+                if (returnedCount > 0) {
+                    setStatus(RETURNED);
+                    onChangeActiveDocument(returnedStatus[0]);
+                } else {
+                    setStatus(WAITING_VALIDATION);
+                    onChangeActiveDocument(waitingValidation[0]);
+                }
+            } else if (returnedStatus.length === 1) {
+                setStatus(WAITING_VALIDATION);
+                onChangeActiveDocument(waitingValidation[0]);
+            } else {
+                onChangeActiveDocument(sortedByStatus[status][0]);
+            }
+
+            return filtered
         },
         initialFn: (response) => {
-            onChangeActiveDocument(response, 0)
+            const filtered = filterResponseFn(response.object);
+            onChangeActiveDocument(filtered.sortedByStatus[status][0])
+
+            return filtered
         },
         serverData: props.serverData.entityFlowProcess,
+        keepOriginalResponse: true,
         enabled: !!identifier,
+    });
+
+    const { mutate, isPending} = useMutation({
+        mutationKey: "submit_process",
+        mutationFn: handleSubmitValidation
     })
+    
+    const { custom_data } = data ?? {}
+    const { sortedByStatus, countStatus, statusProcess } = custom_data ?? {}
+
+    if (statusProcess?.isApproved || props.serverData.isApproved) {
+        return (
+            <div>
+                <ComponentDocumentApproved/>
+            </div>
+        )
+    }
 
     return (
         <div className={"my-15"}>
 
-            <div className="separator separator-dotted separator-content border-dark mt-15 mb-10">
+            <div className="separator separator-dotted separator-content border-dark mt-15 mb-5">
                 <span className="h4">Documentos</span>
             </div>
-            <h5 className={"text-muted fw-normal mb-10"}>
+            <p className={"text-muted fw-normal mb-10 text-center"}>
                 É necessário o envio dos seguintes documentos para realizar seu cadastro:
-            </h5>
+            </p>
+
+            <div className={"d-flex justify-content-end pb-5"}>
+                <span>Status: <strong className={"fw-bold"}> {getStatusProcessText(data?.original_response?.object?.status)}</strong></span>
+            </div>
+
+            <div className={"d-flex justify-content-between  mb-5"}>
+                <div className={"row row-cols-auto align-items-center"}>
+                    {statusToFilter.map((item, index) => {
+
+                        if (!statusDocumentToDisplay()?.includes(item.value)) {
+                            return null;
+                        }
+
+                        if (!statusProcess?.isDraft && item.value === WAITING_VALIDATION && !statusProcess.isReturned) {
+                            statusDocument[item.value].label = "Aguardando validação"
+                        }
+
+
+                        return (
+                            <React.Fragment key={index}>
+                                <ComponentDocumentFilter status={statusDocument[item.value]}
+                                                         count={countStatus[item.value]}
+                                                         activeStatus={status}
+                                                         disabled={countStatus[item.value] === 0}
+                                                         onClick={(key) => {
+                                                             setStatus(key)
+                                                             if (sortedByStatus[key].length > 0) {
+                                                                 onChangeActiveDocument(sortedByStatus[key][0])
+                                                             } else {
+                                                                 onChangeActiveDocument(null)
+                                                             }
+                                                         }}
+                                />
+                            </React.Fragment>
+                        )
+                    })}
+                </div>
+
+                <div>
+                    {((statusProcess?.isDraft || statusProcess.isReturned) && status === WAITING_VALIDATION) &&
+                        <button className={"btn btn-success btn-sm"}
+                                onClick={mutate}
+                                disabled={countStatus[PENDING_UPLOAD] !== 0 || isPending}>
+                            {isPending ? "Iniciando..." : "Iniciar validação"}
+                        </button>
+                    }
+                </div>
+            </div>
 
             <div className="d-flex gap-4">
                 <div className="w-md-300px flex-shrink-0 position-relative">
                     <div className={"position-sticky"} style={{top: 100}}>
-                        <div className="card overflow-auto hover-scroll" style={{maxHeight:"85vh"}}>
-                            <div className={"card-body ps-3 pe-0 py-3"}>
+                        <div className="card hover-scroll-overlay-y" style={{maxHeight: "85vh"}}>
+                            <div className={"card-body ps-5 pe-3 py-3"}>
                                 <ul className="d-flex flex-column gap-3 list-style-none border-bottom-all">
-                                    {data?.object?.entity_documents?.map((item, index) => {
+                                    {sortedByStatus?.[status]?.map((item, index) => {
 
                                         return (
-                                            <li className={``} onClick={() => onChangeActiveDocument(data, index)} key={index}>
-                                                <div className={`${isActiveMenu(item.document_type_id)} text-hover-secondary text-justify cursor-pointer`}>
-                                                    {index + 1}. {item.document_type.title}
-                                                </div>
+                                            <li onClick={() => onChangeActiveDocument(item)} key={index}>
+                                                <p className={`${isActiveMenu(item.document_type_id)} mb-0 fs-7 text-hover-secondary  cursor-pointer`}>
+                                                    <KTIcon
+                                                        name={item.document_type_id === activeDocument?.document_type_id ? "arrow-right" : ""}
+                                                        className={"me-1"}/>
+                                                    {item.document_type.title}
+                                                </p>
                                             </li>
                                         )
                                     })}
@@ -105,32 +274,12 @@ export default function ComponentDocuments(props) {
                     </div>
                 </div>
                 <div className={"flex-grow-1"}>
-                    <div className={`card card-dashed border-${colorBadge}`}>
-                        <div className={`card-header bg-${colorBadge}-subtle border-${colorBadge}`}>
-                            <div className={`card-title text-${colorBadge}-emphasis`}>
-                                {activeDocument?.document_type?.title}
-                            </div>
-                            <div className="card-toolbar">
-                                <BadgeStatus status={activeDocument?.status}/>
-                            </div>
-                        </div>
-                        <div className={"card-body"}>
-                            <p className={"text-justify mb-0"}>
-                                {activeDocument?.document_type?.description}
-                            </p>
-                        </div>
-                    </div>
-
-                    <div className={"mt-10"}>
-                        <ComponentSelectFile onAttachments={(e) => console.log(e)}
-                                             maxFiles={1}
-                        />
-                    </div>
-                    <div className={"d-flex justify-content-end"}>
-                        <button className={"btn btn-sm btn-light-secondary mt-5"}>
-                            Salvar arquivo
-                        </button>
-                    </div>
+                    <ComponentDocumentDetails data={activeDocument}
+                                              color={statusDocument[status].color}
+                                              refetch={refetch}
+                                              activeStatus={status}
+                                              isValidationStarted={statusProcess?.isWaitingValidation || statusProcess.isValidating}
+                    />
                 </div>
             </div>
         </div>
--- src/ui/components/component-resume-entity.jsx
+++ src/ui/components/component-resume-entity.jsx
@@ -24,7 +24,7 @@ export default function ComponentResumeEntity(props) {
             </div>
             <div className="col-6">
                 <div className="d-flex justify-content-end gap-2">
-                    <div className={"border rounded-3 text-end px-5 py-3 position-relative bg-warning-subtle border-warning-subtle"}>
+                    <div className={"border rounded-3 text-end px-5 py-3 position-relative bg-body shadow-sm"}>
                         <KTIcon name={"time"}
                                 style={{fontSize:"3.25rem", zIndex:0, left:5}}
                                 className={"position-absolute bottom-0 text-warning-emphasis mb-1"}
@@ -35,7 +35,7 @@ export default function ComponentResumeEntity(props) {
                         </h2>
                     </div>
 
-                    <div className={"border rounded-3 text-end position-relative px-5 py-3 bg-success-subtle border-success-subtle"}>
+                    <div className={"border rounded-3 text-end position-relative px-5 py-3 bg-body shadow-sm"}>
                         <KTIcon name={"check-circle"}
                                 style={{fontSize:"3.25rem", zIndex:0, left:5}}
                                 className={"position-absolute bottom-0 text-success-emphasis mb-1"}
